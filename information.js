e43e147d3cd8ccced18ff0c97a732e89f9ff91bb
Sabrina KARAKOG <sabrina.karakog@gmail.com>
2020-09-28 23:44:36 +0200

    ajout quete terminee

---
+++ information.js
@@ -0,0 +1,8 @@
+const wilderIdentity = {
+  name: 'Sabrina KARAKOG',
+  campus: 'Lyon',
+};
+
+const myInfos = `Hi I'm ${wilderIdentity.name} from ${wilderIdentity.campus} Campus!`;
+
+module.exports = myInfos;
